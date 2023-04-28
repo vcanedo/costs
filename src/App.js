f8b8a9792cd340b81aae7869d53f52916877dfbc
victor canedo <victorcanedo1@hotmail.com>
2023-04-28 12:20:16 -0300

    added routes and links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
 
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/newproject">New Project</Link>
+      </div>
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/company" element={<Company />} />
+        <Route exact path="/newproject" element={<NewProject />} />
+      </Routes>
+    </Router>
   );
 }
 
