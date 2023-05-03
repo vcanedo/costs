2ef514830c3f26083526399029fa95ea9e69dc3c
victor canedo <victorcanedo1@hotmail.com>
2023-05-03 10:19:19 -0300

    in ProjectForm.js added api fetch in method:GET from localhost:5000/categories, and set that data from the api to setCategories method from useState functionality

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
@@ -5,6 +7,21 @@ import SubmitButton from "../form/SubmitButton";
 import styles from "./ProjectForm.module.css";
 
 function ProjectForm({btnText}) {
+
+  const [categories, setCategories] = useState([]);
+
+  fetch("http://localhost:5000/categories", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  .then((resp) => resp.json())
+  .then((data) => {
+    setCategories(data)
+  })
+  .catch((err) => console.log(err))
+
   return (
     <form className={styles.form}>
       <Input
