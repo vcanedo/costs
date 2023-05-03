8ec17061a59565336e2ce2a1b6740b442fe78831
victor canedo <victorcanedo1@hotmail.com>
2023-05-03 10:25:16 -0300

    in Select.js added options to be shown via an options.map() to show each with its data and id

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -6,9 +6,13 @@ function Select ({ text, name, options, handleOnChange, value }) {
       <label htmlFor={name}>{text}:</label>
       <select name={name} id={name}>
         <option>Select an option</option>
+        {options.map((option) => (
+          <option value={option.id} key={option.id}>{option.name}</option>
+        ))}
       </select>
     </div>
   )
 }
 
 export default Select;
+
