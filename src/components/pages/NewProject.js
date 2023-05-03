5582faed8e9dee11076d70ce44e6d9d01251ce4e
victor canedo <victorcanedo1@hotmail.com>
2023-05-03 11:02:59 -0300

    in NewProject.js added api fetch on http://localhost:5000/projects to method:POST its data

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,7 +1,31 @@
+import { useHistory } from "react-router-dom";
+
 import ProjectForm from "../project/ProjectForm";
 import styles from "./NewProject.module.css";
 
 function NewProject() {
+
+  const history = useHistory();
+
+  const createPost = (project) => {
+    // initialize cost and services
+    project.cost = 0;
+    project.services = [];
+
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data)
+      // redirect
+    })
+    .catch((err) => console.log(err))
+  }
+
   return (
     <div className={styles.newproject_container}>
       <h1>Create Project</h1>
