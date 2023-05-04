53c8e371f7de277701a1912ac1b14e2a45e08c26
victor canedo <victorcanedo1@hotmail.com>
2023-05-04 12:15:17 -0300

    added modularity to the message

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,10 +1,20 @@
+import { useLocation } from "react-router-dom";
+
 import Message from "../layout/Message";
 
 function Projects() {
+
+  const location = useLocation()
+
+  let message = ''
+  if(location.state) {
+    message = location.state.message
+  }
+
   return (
     <div>
       <h1>My Projects</h1>
-      <Message msg="some message" type="error"/>
+      {message && <Message type="success" msg={message} />}
     </div>
   );
 }
