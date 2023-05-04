b01c03e0f661f13a282aede4e60ee6c5fc074b83
victor canedo <victorcanedo1@hotmail.com>
2023-05-04 12:12:59 -0300

    added useEffect to the message for it to have a timer when show, for it to dissapear after some time

--- src/components/layout/Message.js
+++ src/components/layout/Message.js
@@ -6,6 +6,20 @@ function Message({ type, msg }) {
 
   const [visible, setVisible] = useState(false);
 
+  useEffect(() => {
+    if(!msg) {
+      setVisible(false)
+      return
+    }
+    setVisible(true)
+
+    const timer = setTimeout(() => {
+      setVisible(false)
+    }, 3000)
+
+    return () => clearTimeout(timer)
+  }, [msg])
+
   return (
     <>
       {visible && (
