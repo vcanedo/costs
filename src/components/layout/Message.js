916ddb5c1a92b1964e51eafe4cc0d1fb23743525
victor canedo <victorcanedo1@hotmail.com>
2023-05-04 12:04:13 -0300

    added useState for the message for it to be only visible if visible = true

--- src/components/layout/Message.js
+++ src/components/layout/Message.js
@@ -1,8 +1,17 @@
+import { useState, useEffect } from "react";
+
 import styles from "./Message.module.css";
 
 function Message({ type, msg }) {
+
+  const [visible, setVisible] = useState(false);
+
   return (
-    <div className={`${styles.message} ${styles[type]}`}>{msg}</div>
+    <>
+      {visible && (
+        <div className={`${styles.message} ${styles[type]}`}>{msg}</div>
+      )}
+    </>
   )
 }
 
