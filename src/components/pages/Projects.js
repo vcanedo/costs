4a72446799a5467efd43ce33dd131c3b5048abea
victor canedo <victorcanedo1@hotmail.com>
2023-05-05 12:02:07 -0300

    added LinkButton component to projects page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,9 +1,10 @@
 import { useLocation } from "react-router-dom";
 
 import Message from "../layout/Message";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
 
 import styles from "./Projects.module.css";
-import Container from "../layout/Container";
 
 function Projects() {
 
@@ -18,7 +19,7 @@ function Projects() {
     <div>
       <div>
         <h1>My Projects</h1>
-        <a href="#">New Project</a>
+        <LinkButton to="/newproject" text="Create Project" />
       </div>
       {message && <Message type="success" msg={message} />}
       <Container customClass="start">
