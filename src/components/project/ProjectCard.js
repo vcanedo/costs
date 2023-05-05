bcae52c333c31871af076cacfa5dbd74cfca737d
victor canedo <victorcanedo1@hotmail.com>
2023-05-05 12:41:20 -0300

    added css to ProjectCard.js

--- src/components/project/ProjectCard.js
+++ src/components/project/ProjectCard.js
@@ -7,7 +7,19 @@ import { BsPencil, BsFillTrashFill } from "react-icons/bs";
 
 function ProjectCard({id, name, budget, category, handleRemove}) {
   return (
-    <p>project</p>
+    <div className={styles.project_card}>
+      <h4>{name}</h4>
+      <p>
+        <span>Budget:</span> ${budget}
+      </p>
+      <p className={styles.category_text}>
+        <span className={`${styles[category.toLowerCase()]}`}></span> {category}
+      </p>
+      <div>
+        <p>Edit</p>
+        <p>Remove</p>
+      </div>
+    </div>
   )
 }
 
