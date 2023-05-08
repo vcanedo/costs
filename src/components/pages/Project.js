54a5b9c9ea59ee256cb9c9d155441fba4c278143
victor canedo <victorcanedo1@hotmail.com>
2023-05-08 13:44:04 -0300

    added project details and button to add project with css

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,10 +3,14 @@ import styles from "./Project.module.css";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
+import Loading from "../layout/Loading";
+import Container from "../layout/Container";
+
 function Project() {
   const { id } = useParams()
 
   const [project, setProject] = useState([])
+  const [showProjectForm, setShowProjectForm] = useState(false)
 
   useEffect(() => {
 
@@ -23,10 +27,45 @@ function Project() {
 
   }, [id])
 
+  function toggleProjectForm() {
+    setShowProjectForm(!showProjectForm)
+  }
+
   return (
-    <div>
-      <h1>Project</h1>
-    </div>
+    <>
+      {project.name ? (
+        <div className={styles.project_details}>
+        <Container customClass="column">
+          <div className={styles.details_container}>
+            <h1>Project: {project.name}</h1>
+            <button className={styles.btn} onClick={toggleProjectForm}>
+              {!showProjectForm ? 'Edit project' : 'Close'}
+            </button>
+            {!showProjectForm ? (
+              <div className={styles.project_info}>
+                <p>
+                  <span>Category:</span> {project.category.name}
+                </p>
+                <p>
+                  <span>Budget:</span> ${project.budget}
+                </p>
+                <p>
+                  <span>Utilized budget:</span> ${project.cost}
+                </p>
+              </div>
+            ) : (
+              <div className={styles.project_info}>
+                <p>form</p>
+              </div>
+            )}
+          </div>
+        </Container>
+      </div>
+
+      ):(
+        <Loading />
+      )}
+    </>
   )
 }
 
