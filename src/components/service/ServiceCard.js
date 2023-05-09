e37f31e6097754e38224ee86684ec1ac10dd7df4
victor canedo <victorcanedo1@hotmail.com>
2023-05-09 13:52:55 -0300

    added description and remove button

--- src/components/service/ServiceCard.js
+++ src/components/service/ServiceCard.js
@@ -1,9 +1,24 @@
 import styles from '../project/ProjectCard.module.css'
 
+import { BsFillTrashFill } from 'react-icons/bs';
+
 function ServiceCard({id, name, cost, description, handelRemove}) {
+
+  const remove = (e) => {}
+
   return (
     <div className={styles.project_card}>
       <h4>{name}</h4>
+      <p>
+        <span>Total cost:</span> ${cost}
+      </p>
+      <p>{description}</p>
+      <div className={styles.project_card_actions}>
+        <button onClick={remove}>
+          <BsFillTrashFill />
+          Delete
+        </button>
+      </div>
     </div>
   )
 }
